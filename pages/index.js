44bab223887233c80c3a60ee0f2d016c04a890f6
Hendrik Pils <hendrik-pils@gmx.de>
2023-12-15 16:35:22 +0100

    user story 3

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,7 @@
-import useSWR from "swr";
-import { useRouter } from "next/router";
 import ArtPieces from "@/components/ArtPieces";
 import Spotlight from "@/components/Spotlight";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
-export default function HomePage() {
-  const { data, error, isLoading, mutate } = useSWR(
-    `https://example-apis.vercel.app/api/art`,
-    fetcher
-  );
-
-  if (error) {
-    return <h1>Oops.... ERRORRRRR.</h1>;
-  }
-  if (isLoading) {
-    return <h1>Loading...</h1>;
-  }
-  if (!data) return <p>no data</p>;
-
-  console.log(data);
-
+export default function SpotlightPage({ data }) {
   function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -32,7 +13,7 @@ export default function HomePage() {
   return (
     <div>
       <h1>Art Gallery App</h1>
-      <ArtPieces pieces={data}></ArtPieces>
+      {/* <ArtPieces pieces={data}></ArtPieces> */}
       <Spotlight
         image={data[randomNumber].imageSource}
         artist={data[randomNumber].artist}
